1b24af4f40d3016975215c2e01f749e4495fb89d
Pangfeng Liu <pangfeng.liu@gmail.com>
2020-05-22 15:36:45 +0800

    update API

--- OpenCL-intermediate/clCreateCommandQueue.h
+++ OpenCL-intermediate/clCreateCommandQueue.h
@@ -1,6 +1,9 @@
 cl_command_queue 
-clCreateCommandQueue(cl_context context,
-		     cl_device_id device,
-		     cl_command_queue_properties 
-		     properties,
-		     cl_int *errcode_ret);
+clCreateCommandQueueWithProperties(
+	  cl_context context,
+	  cl_device_id device,
+	  const cl_queue_properties 
+	  *properties,
+	  cl_int *errcode_ret);
+
+
