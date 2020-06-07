daa98d271ae0b842a69e87e791cbb2627bdcfa7e
Pangfeng Liu <pangfeng.liu@gmail.com>
2020-06-07 10:22:41 +0800

    add CUDA materials

---
+++ CUDA/globalID.cu
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <cuda.h>
+#define N 8
+__global__ void hello(int int_array[N])
+{
+  int_array[threadIdx.x] *= threadIdx.x;
+}
+
+int main(void)
+{
+  int *device_int_array;
+  int *host_int_array;
+  int i;
+  int size = sizeof(int) * N;
+  cudaMalloc((void **)&device_int_array, size);
+  host_int_array = (int *)malloc(size);
+  for (i = 0; i < N; i++)
+    host_int_array[i] = i;
+  cudaMemcpy(device_int_array, host_int_array, size,
+	     cudaMemcpyHostToDevice);
+  hello <<< 1, N >>> (device_int_array);
+  cudaMemcpy(host_int_array, device_int_array, size,
+	     cudaMemcpyDeviceToHost);
+  for (i = 0; i < N; i++)
+    printf("host_int_array[%d] = %d\n", i, host_int_array[i]);
+  cudaFree(device_int_array);
+  free(host_int_array);
+}
