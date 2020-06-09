5e46ec566c20dd8d2331a2a6d8908863fe2849aa
Pangfeng Liu <pangfeng.liu@gmail.com>
2020-06-09 15:41:54 +0800

    add blockIndx

--- CUDA/globalID2D.cu
+++ CUDA/globalID2D.cu
@@ -2,30 +2,32 @@
 #include <cuda.h>
 
 #define N 4
-__global__ void hello(int int_array[N][N])
+__global__ void hello(int int_array[4][N][N])
 {
-  int_array[threadIdx.x][threadIdx.y]
-    *= (threadIdx.x + threadIdx.y);
+  int_array[blockIdx.x][threadIdx.x][threadIdx.y]
+    *= ((threadIdx.x + threadIdx.y) * blockIdx.x);
 }
 
 int main(void)
 {
   int *device_int_array;
-  int size = sizeof(int) * N * N;
-  int host_int_array[N][N];
-  dim3 blocks (N,N);
+  int size = sizeof(int) * N * N * 4;
+  int host_int_array[4][N][N];
+  dim3 blocks (N, N);
   cudaMalloc((void **)&device_int_array, size);
   for (int i = 0; i < N; i++)
     for (int j = 0; j < N; j++)
-      host_int_array[i][j] = i + j;
+      for (int k = 0; k < 4; k++)
+	host_int_array[k][i][j] = i + j;
   cudaMemcpy(device_int_array, host_int_array, size,
 	     cudaMemcpyHostToDevice);
-  hello <<< 1, blocks >>> ((int (*)[N])device_int_array);
+  hello <<< 4, blocks >>> ((int (*)[N][N])device_int_array);
   cudaMemcpy(host_int_array, device_int_array, size,
 	     cudaMemcpyDeviceToHost);
-  for (int i = 0; i < N; i++)
-    for (int j = 0; j < N; j++)
-      printf("host_int_array[%d][%d] = %d\n", i, j,
-	     host_int_array[i][j]);
+  for (int k = 0; k < 4; k++)
+    for (int i = 0; i < N; i++)
+      for (int j = 0; j < N; j++)
+	printf("host_int_array[%d][%d][%d] = %d\n", k, i, j,
+	       host_int_array[k][i][j]);
   cudaFree(device_int_array);
 }
